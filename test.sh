0ea13953b4ae5054cec679d26bdca2b68efa1e05
2da-tuba <39460286+2da-tuba@users.noreply.github.com>
2021-01-20 15:36:39 +0900

    レガシーな部分(バッククォート)や文字列をクォートで囲むなどの改善

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # test for kat
 
 mkdir kat_test
-cd kat_test
+cd kat_test || exit
 
 touch kattest.fuga.hoge
 touch kattest
@@ -11,7 +11,7 @@ touch kattest.piyo
 
 ../kat kattest.fuga.hoge
 
-if [ -e kattest`date "+(%Y年%m月%d日版)"`.fuga.hoge ]; then
+if [ -e "kattest$(date "+(%Y年%m月%d日版)").fuga.hoge" ]; then
   echo "kattest.fuga.hoge ok"
 else
   echo "kattest.fuga.hoge ng"
@@ -19,7 +19,7 @@ fi
 
 ../kat kattest
 
-if [ -e kattest`date "+(%Y年%m月%d日版)"` ]; then
+if [ -e "kattest$(date "+(%Y年%m月%d日版)")" ]; then
   echo "kattest ok"
 else
   echo "kattest ng"
@@ -27,7 +27,7 @@ fi
 
 ../kat kattest.piyo
 
-if [ -e kattest`date "+(%Y年%m月%d日版)"`.piyo ]; then
+if [ -e "kattest$(date "+(%Y年%m月%d日版)").piyo" ]; then
   echo "kattest.piyo ok"
 else
   echo "kattest.piyo ng"
