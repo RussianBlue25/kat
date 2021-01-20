9051d2882d0ec8cc07916dc3e1cbb7ac86979ac9
2da-tuba <39460286+2da-tuba@users.noreply.github.com>
2021-01-21 05:25:24 +0900

    日付付け替えのテストの追加

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
 # test for kat
 
@@ -8,6 +9,9 @@ cd kat_test || exit
 touch kattest.fuga.hoge
 touch kattest
 touch kattest.piyo
+touch "kattest2(2000年01月01日版).fuga.hoge"
+touch "kattest2(2000年01月01日版)"
+touch "kattest2(2000年01月01日版).piyo"
 
 ../kat kattest.fuga.hoge
 
@@ -15,6 +19,7 @@ if [ -e "kattest$(date "+(%Y年%m月%d日版)").fuga.hoge" ]; then
   echo "kattest.fuga.hoge ok"
 else
   echo "kattest.fuga.hoge ng"
+  rm -rf kat_test
   exit 1
 fi
 
@@ -24,6 +29,7 @@ if [ -e "kattest$(date "+(%Y年%m月%d日版)")" ]; then
   echo "kattest ok"
 else
   echo "kattest ng"
+  rm -rf kat_test
   exit 1
 fi
 
@@ -33,6 +39,37 @@ if [ -e "kattest$(date "+(%Y年%m月%d日版)").piyo" ]; then
   echo "kattest.piyo ok"
 else
   echo "kattest.piyo ng"
+  rm -rf kat_test
+  exit 1
+fi
+
+../kat "kattest2(2000年01月01日版).fuga.hoge"
+
+if [ -e "kattest2$(date "+(%Y年%m月%d日版)").fuga.hoge" ]; then
+  echo "kattest2(2000年01月01日版).fuga.hoge ok"
+else
+  echo "kattest2(2000年01月01日版).fuga.hoge ng"
+  rm -rf kat_test
+  exit 1
+fi
+
+../kat "kattest2(2000年01月01日版)"
+
+if [ -e "kattest2$(date "+(%Y年%m月%d日版)")" ]; then
+  echo "kattest2(2000年01月01日版) ok"
+else
+  echo "kattest2(2000年01月01日版) ng"
+  rm -rf kat_test
+  exit 1
+fi
+
+../kat "kattest2(2000年01月01日版).piyo"
+
+if [ -e "kattest2$(date "+(%Y年%m月%d日版)").piyo" ]; then
+  echo "kattest2(2000年01月01日版).piyo ok"
+else
+  echo "kattest2(2000年01月01日版).piyo ng"
+  rm -rf kat_test
   exit 1
 fi
 
