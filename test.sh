5c6dc8e0d5944e66a7a2d99ba4988c245f4fba8b
2da-tuba <39460286+2da-tuba@users.noreply.github.com>
2021-01-23 00:41:39 +0900

    同一日の区別化とテストの書き換え

--- test.sh
+++ test.sh
@@ -8,9 +8,6 @@ cd kat_test || exit
 touch kattest.fuga.hoge
 touch kattest
 touch kattest.piyo
-touch "kattest2(2000年01月01日版).fuga.hoge"
-touch "kattest2(2000年01月01日版)"
-touch "kattest2(2000年01月01日版).piyo"
 
 ../kat kattest.fuga.hoge
 
@@ -18,7 +15,15 @@ if [ -e "kattest$(date "+(%Y年%m月%d日版)").fuga.hoge" ]; then
   echo "kattest.fuga.hoge ok"
 else
   echo "kattest.fuga.hoge ng"
-  rm -rf kat_test
+  exit 1
+fi
+
+../kat kattest.fuga.hoge
+
+if [ -e "kattest$(date "+(%Y年%m月%d日%H時%M分%S秒版)").fuga.hoge" ]; then
+  echo "kattest$(date "+(%Y年%m月%d日版)").fuga.hoge ok"
+else
+  echo "kattest$(date "+(%Y年%m月%d日版)").fuga.hoge ng"
   exit 1
 fi
 
@@ -28,7 +33,15 @@ if [ -e "kattest$(date "+(%Y年%m月%d日版)")" ]; then
   echo "kattest ok"
 else
   echo "kattest ng"
-  rm -rf kat_test
+  exit 1
+fi
+
+../kat kattest
+
+if [ -e "kattest$(date "+(%Y年%m月%d日%H時%M分%S秒版)")" ]; then
+  echo "kattest$(date "+(%Y年%m月%d日版)") ok"
+else
+  echo "kattest$(date "+(%Y年%m月%d日版)") ng"
   exit 1
 fi
 
@@ -38,37 +51,15 @@ if [ -e "kattest$(date "+(%Y年%m月%d日版)").piyo" ]; then
   echo "kattest.piyo ok"
 else
   echo "kattest.piyo ng"
-  rm -rf kat_test
   exit 1
 fi
 
-../kat "kattest2(2000年01月01日版).fuga.hoge"
+../kat kattest.piyo
 
-if [ -e "kattest2$(date "+(%Y年%m月%d日版)").fuga.hoge" ]; then
-  echo "kattest2(2000年01月01日版).fuga.hoge ok"
+if [ -e "kattest$(date "+(%Y年%m月%d日%H時%M分%S秒版)").piyo" ]; then
+  echo "kattest$(date "+(%Y年%m月%d日版)").piyo ok"
 else
-  echo "kattest2(2000年01月01日版).fuga.hoge ng"
-  rm -rf kat_test
-  exit 1
-fi
-
-../kat "kattest2(2000年01月01日版)"
-
-if [ -e "kattest2$(date "+(%Y年%m月%d日版)")" ]; then
-  echo "kattest2(2000年01月01日版) ok"
-else
-  echo "kattest2(2000年01月01日版) ng"
-  rm -rf kat_test
-  exit 1
-fi
-
-../kat "kattest2(2000年01月01日版).piyo"
-
-if [ -e "kattest2$(date "+(%Y年%m月%d日版)").piyo" ]; then
-  echo "kattest2(2000年01月01日版).piyo ok"
-else
-  echo "kattest2(2000年01月01日版).piyo ng"
-  rm -rf kat_test
+  echo "kattest$(date "+(%Y年%m月%d日版)").piyo ng"
   exit 1
 fi
 
